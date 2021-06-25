2703ea2e6bff90b693c23ecf32e425c10a500952
Edozie <phynnedo@gmail.com>
2021-06-25 16:57:37 +0100

    Initial Commit

---
+++ script/script.js
@@ -0,0 +1,6 @@
+//Variable Declarations
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalButton = document.querySelector('[data-equal]');
+const deleteButton = document.querySelector('[data-delete]');
+const resetButton = document.querySelector('[data-reset]');
