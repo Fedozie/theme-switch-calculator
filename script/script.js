3e39e32f0f5e3b15735bcb7494c2eae57eefcf12
Edozie <phynnedo@gmail.com>
2021-06-29 11:04:33 +0100

    Second Commit

--- script/script.js
+++ script/script.js
@@ -4,3 +4,16 @@ const operationButtons = document.querySelectorAll('[data-operation]');
 const equalButton = document.querySelector('[data-equal]');
 const deleteButton = document.querySelector('[data-delete]');
 const resetButton = document.querySelector('[data-reset]');
+const btns = document.querySelectorAll('button');
+
+
+
+//Listening for click event on the buttons
+btns.forEach((btn)=> {
+    btn.addEventListener('click', () => {
+        btn.style.transform = 'translateY(5px)';
+        setTimeout(() => {
+            btn.style.transform = 'translateY(0px)'
+        }, 400)
+    })
+})
