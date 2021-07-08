5ec2042b56d4d51567f550edf4c7358bf108e758
Edozie <phynnedo@gmail.com>
2021-07-08 15:25:47 +0100

    Fifth Commit

--- script/script.js
+++ script/script.js
@@ -1,15 +1,69 @@
+let solved;
+let error;
+
+//Function to clear/reset the calculator
+function reset() {
+    const input = document.querySelector('.current-output');
+    input.innerHTML = '';
+    document.querySelector(".previous-output").innerHTML = '';
+}
+
+//Function to append number to the input
+function appendNumber(e) {
+    const previousInput = document.querySelector('.previous-output');
+    const currentInput = document.querySelector('.current-output');
+    if (solved || error) {
+        previousInput.innerHTML = '';
+        currentInput.innerHTML = '';
+        solved = false;
+        error = false;
+    }
+    document.querySelector(".current-output").innerHTML += e
+    if (e === '*' || e === '+' || e === '-' || e === '.' || e === '/') {
+        previousInput.innerHTML += currentInput.innerHTML;
+        currentInput.innerHTML = '';
+    }
+}
+
+//Function that solves the input and displays a result
+function solve() {
+    try {
+        let x = document.querySelector(".previous-output").innerHTML;
+        let currentOutput = document.querySelector('.current-output');
+        if (currentOutput) {
+            x += currentOutput.innerHTML
+        }
+        let y = eval(x);
+        document.querySelector(".previous-output").innerHTML = x;
+        document.querySelector(".current-output").innerHTML = y;
+        solved = true;
+    }
+    catch (err) {
+        //In the case, where the input is not a correct mathematical error. This error message would be returned
+        document.querySelector(".previous-output").innerHTML = '';
+        document.querySelector(".current-output").innerHTML = "Math Error";
+        error = true;
+    }
+}
+
+function dlt() {
+    if(solved || error){
+        return reset()
+    }
+    let input = document.querySelector('.current-output');
+    if(input){
+        const del = input.innerHTML.split('');
+        let newInput = del.slice(0, -1);
+        input.innerHTML = newInput.join('')
+    }
+}
+
 //Variable Declarations
-const radioBtns = document.querySelectorAll('input[type=radio]');
 const firstThemeBtn = document.querySelector('#first');
 const secondThemeBtn = document.querySelector('#second');
 const thirdThemeBtn = document.querySelector('#third');
 const ballBtn = document.querySelector('.switch-ball');
-const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operation]');
-const equalButton = document.querySelector('[data-equal]');
-const deleteButton = document.querySelector('[data-delete]');
-const resetButton = document.querySelector('[data-reset]');
-const btns = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('button');
 
 //Toggling amongst the themes
 firstThemeBtn.addEventListener('change', () => {
@@ -31,12 +85,11 @@ thirdThemeBtn.addEventListener('change', () => {
 })
 
 //Listening for click event on the buttons
-btns.forEach((btn)=> {
-    btn.addEventListener('click', () => {
-        btn.style.transform = 'translateY(5px)';
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        button.style.transform = 'translateY(5px)';
         setTimeout(() => {
-            btn.style.transform = 'translateY(0px)'
+            button.style.transform = 'translateY(0px)'
         }, 150)
     })
 })
-
