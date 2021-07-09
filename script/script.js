ea892355a95a81ec7ae9c956da19f18208603957
Edozie <phynnedo@gmail.com>
2021-07-09 12:45:08 +0100

    Sixth Commit

--- script/script.js
+++ script/script.js
@@ -1,24 +1,24 @@
 let solved;
 let error;
 
-//Function to clear/reset the calculator
+//Function to clear/reset the calculator when the reset button has been clicked
 function reset() {
     const input = document.querySelector('.current-output');
     input.innerHTML = '';
-    document.querySelector(".previous-output").innerHTML = '';
+    document.querySelector('.previous-output').innerHTML = '';
+    document.querySelector('.current-output').style.color = "#fff";
 }
 
-//Function to append number to the input
+//Function to append number or operators to the input when the buttons containing numbers or operators are clicked
 function appendNumber(e) {
     const previousInput = document.querySelector('.previous-output');
     const currentInput = document.querySelector('.current-output');
     if (solved || error) {
-        previousInput.innerHTML = '';
-        currentInput.innerHTML = '';
+        reset();
         solved = false;
         error = false;
     }
-    document.querySelector(".current-output").innerHTML += e
+    document.querySelector('.current-output').innerHTML += e;
     if (e === '*' || e === '+' || e === '-' || e === '.' || e === '/') {
         previousInput.innerHTML += currentInput.innerHTML;
         currentInput.innerHTML = '';
@@ -31,30 +31,35 @@ function solve() {
         let x = document.querySelector(".previous-output").innerHTML;
         let currentOutput = document.querySelector('.current-output');
         if (currentOutput) {
-            x += currentOutput.innerHTML
+            x += currentOutput.innerHTML;
         }
         let y = eval(x);
-        document.querySelector(".previous-output").innerHTML = x;
-        document.querySelector(".current-output").innerHTML = y;
+        document.querySelector('.previous-output').innerHTML = x;
+        document.querySelector('.current-output').innerHTML = y;
         solved = true;
     }
     catch (err) {
-        //In the case, where the input is not a correct mathematical error. This error message would be returned
-        document.querySelector(".previous-output").innerHTML = '';
-        document.querySelector(".current-output").innerHTML = "Math Error";
+        //In the case, where the input is not a correct mathematical error. An error message would be returned
+        document.querySelector('.previous-output').innerHTML = '';
+        document.querySelector('.current-output').innerHTML = "Math Error!";
+        document.querySelector('.current-output').style.color = "#c5404c";
         error = true;
     }
 }
 
+//Function that deletes the input starting from the back
 function dlt() {
     if(solved || error){
-        return reset()
-    }
+        return reset();
+    };
     let input = document.querySelector('.current-output');
-    if(input){
+    if(input.innerHTML){
         const del = input.innerHTML.split('');
         let newInput = del.slice(0, -1);
-        input.innerHTML = newInput.join('')
+        input.innerHTML = newInput.join('');
+    }else{
+        input.innerHTML = document.querySelector('.previous-output').innerHTML;
+        document.querySelector('.previous-output').innerHTML = '';
     }
 }
 
@@ -81,7 +86,7 @@ secondThemeBtn.addEventListener('change', () => {
 thirdThemeBtn.addEventListener('change', () => {
     ballBtn.style.left = '56px';
     document.querySelector('body').classList.add('neon');
-    document.querySelector('body').classList.remove('light')
+    document.querySelector('body').classList.remove('light');
 })
 
 //Listening for click event on the buttons
@@ -90,6 +95,6 @@ buttons.forEach((button) => {
         button.style.transform = 'translateY(5px)';
         setTimeout(() => {
             button.style.transform = 'translateY(0px)'
-        }, 150)
-    })
+        }, 150);
+    });
 })
